67cc8cf820da4270ad596527202b6479b6549d15
micheal <micheal.xudb@foxmail.com>
2013-03-07 09:32:54 +0800

    add 404 test page

--- sohu_news/urls.py
+++ sohu_news/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'news.views.news_by_time_mako'),
     url(r'^cate/', 'news.views.news_by_cate_mako'),
     url(r'^detail/(\d+)/$', 'news.views.news_detail_mako'),
     url(r'^newslike/(\d+)$', 'news.views.news_like'),
     url(r'^newsdislike/(\d+)$', 'news.views.news_dislike'),
     url(r'^about/$', 'news.views.about'),
+    url('^404test/$', direct_to_template, {'template': '404.html'}),
     # url(r'^sohu_news/', include('sohu_news.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
